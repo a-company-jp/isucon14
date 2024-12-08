2c168f91ddad96e79dcb5911be546dc23bb710ba
Shion Ichikawa <shion1305@gmail.com>
2024-12-08 12:43:03 +0900

    make up

--- sql/migrate/000002_migrate.down.sql
+++ sql/migrate/000002_migrate.down.sql
@@ -1,7 +1,7 @@
-DROP INDEX IF EXISTS idx_users_access_token;
-DROP INDEX IF EXISTS idx_chairs_is_active;
-DROP INDEX IF EXISTS idx_chairs_owner_id;
-DROP INDEX IF EXISTS idx_ride_statuses_ride_id_created_at;
-DROP INDEX IF EXISTS idx_rides_user_id;
-DROP INDEX IF EXISTS idx_rides_chair_id;
-DROP INDEX IF EXISTS idx_chair_locations_chair_id_created_at;
+DROP INDEX idx_users_access_token ON users;
+DROP INDEX idx_chairs_is_active ON chairs;
+DROP INDEX idx_chairs_owner_id ON chairs;
+DROP INDEX idx_ride_statuses_ride_id_created_at ON ride_statuses;
+DROP INDEX idx_rides_user_id ON rides;
+DROP INDEX idx_rides_chair_id ON rides;
+DROP INDEX idx_chair_locations_chair_id_created_at ON chair_locations;
