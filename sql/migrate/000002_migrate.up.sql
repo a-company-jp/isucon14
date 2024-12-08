3cc8f132464098c096dd29a1dea62cce7244a2d8
Shion Ichikawa <shion1305@gmail.com>
2024-12-08 12:46:48 +0900

    deprecate deploy-db

--- sql/migrate/000002_migrate.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE INDEX idx_users_access_token ON users(access_token);
-CREATE INDEX idx_chairs_is_active ON chairs(is_active);
-CREATE INDEX idx_chairs_owner_id ON chairs(owner_id);
-CREATE INDEX idx_ride_statuses_ride_id_created_at ON ride_statuses(ride_id, created_at DESC);
-CREATE INDEX idx_rides_user_id ON rides(user_id);
-CREATE INDEX idx_rides_chair_id ON rides(chair_id);
-CREATE INDEX idx_chair_locations_chair_id_created_at ON chair_locations(chair_id, created_at DESC);
