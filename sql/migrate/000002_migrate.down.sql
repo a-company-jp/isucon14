cdb12a7537da7135da478629635aae9967c0ddfa
Ishigami100 <yamahaya5151@icloud.com>
2024-12-08 12:01:35 +0900

    drop用

--- sql/migrate/000002_migrate.down.sql
+++ sql/migrate/000002_migrate.down.sql
@@ -1,1 +1,7 @@
--- EMPTY
+DROP INDEX IF EXISTS idx_users_access_token;
+DROP INDEX IF EXISTS idx_chairs_is_active;
+DROP INDEX IF EXISTS idx_chairs_owner_id;
+DROP INDEX IF EXISTS idx_ride_statuses_ride_id_created_at;
+DROP INDEX IF EXISTS idx_rides_user_id;
+DROP INDEX IF EXISTS idx_rides_chair_id;
+DROP INDEX IF EXISTS idx_chair_locations_chair_id_created_at;
