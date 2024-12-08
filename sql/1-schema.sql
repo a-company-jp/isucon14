11c93b86073f114fae8b3298e30e590022f2f7f6
Shion Ichikawa <shion1305@gmail.com>
2024-12-08 13:10:56 +0900

    Revert "create index idx_chair_locations_chair_id_created_at_lat_lon"
    This reverts commit 47ca6f2c2c523bb79a081f8ab6950b0621778c27.

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -143,8 +143,3 @@ CREATE INDEX idx_ride_statuses_ride_id_created_at ON ride_statuses(ride_id, crea
 CREATE INDEX idx_rides_user_id ON rides(user_id);
 CREATE INDEX idx_rides_chair_id ON rides(chair_id);
 CREATE INDEX idx_chair_locations_chair_id_created_at ON chair_locations(chair_id, created_at DESC);
-
-
-CREATE INDEX idx_chair_locations_chair_id_created_at_lat_lon
-ON chair_locations (chair_id, created_at, latitude, longitude);
-
